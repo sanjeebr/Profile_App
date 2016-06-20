03a53d245d3877ca202752e0db59cb25ec7215cd
sanjeebr <sanjeebr@mindfiresolutions.com>
2016-06-20 09:59:18 +0530

    form using html css php

---
+++ config/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Constants file.
+ */
+
+// Path for profile pic.
+define('PROFILE_PIC', 'img/profile_pic/');
+
+// Path for default profile pic .
+define('DEFAULT_PROFILE_PIC', 'img/');
---
+++ delete.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Delete employee data file.
+ */
+
+require_once('config/database_config.php');
+require_once('config/constants.php');
+
+if (isset($_GET['emp_id']) && preg_match('/^[0-9]*$/', $_GET['emp_id'])) {
+    $emp_id = $conn->real_escape_string($_GET['emp_id']);
+
+    $sql_query = "SELECT employee.photo AS photo
+        FROM employee
+        WHERE employee.id = $emp_id";
+
+    $result = mysqli_query($conn, $sql_query);
+    $row = mysqli_fetch_assoc($result);
+
+    $photo = PROFILE_PIC . $row['photo'];
+
+    // Delete data from address table
+    $sql = "DELETE FROM address WHERE employee_id = $emp_id";
+    $result = mysqli_query($conn, $sql);
+
+    if (FALSE === $result) {
+        header('Location: error.php');
+    }
+
+    // Delete data from employee table.
+    $sql = "DELETE FROM employee WHERE id = $emp_id";
+	$result = mysqli_query($conn, $sql);
+
+    if (FALSE === $result) {
+        header('Location: error.php');
+    }
+
+    // Delete profile pic
+    unlink($photo);
+}
+
+header('Location: employee.php');
